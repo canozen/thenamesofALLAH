a2e7501461a312388e3cc7c47328b8e67786e3d7
Can ÖZEN <canozen3@gmail.com>
2022-10-07 18:16:11 +0300

    The Names of ALLAH

---
+++ ALLAHın isimleri/Program.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALLAHın_isimleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("Güzel ALLAH'ımızın birbirinden güzel isimlerini içeren uygulamaya hoş geldiniz.\n");
+            Console.Write("1. ALLAH           34. EL- HALÎM         67. EL- MÜHEYMİN\n");
+            Console.Write("2. EL- ADL         35. EL- HAMÎD         68. EL- MÜMÎT\n");
+            Console.Write("3. EL- AFÜVV       36. EL- HASÎB         69. EL- MÜNTEKIM\n");
+            Console.Write("4. EL- ÂHİR        37. EL- HAYY          70. EL- MÜTEÂLÎ\n");
+            Console.Write("5. EL- ALÎM        38. EL- KÂBID         71. EL- MÜTEKEBBİR\n");
+            Console.Write("6. EL- ALİYY       39. EL- KÂDİR         72. EL- MÜZİL\n");
+            Console.Write("7. EL- AZÎM        40. EL- KAHHÂR        73. EN- NÂFİ\n");
+            Console.Write("8. EL- AZÎZ        41. EL- KAVİYY        74. EN- NÛR\n");
+            Console.Write("9. EL- BÂİS        42. EL- KAYYÛM        75. ER- RÂFİ\n");
+            Console.Write("10. EL- BÂKÎ       43. EL- KEBÎR         76. ER- RAHÎM\n");
+            Console.Write("11. EL- BÂRİ       44. EL- KERÎM         77. ER- RAHMÂN\n");
+            Console.Write("12. EL- BÂSIT      45. EL- KUDDÛS        78. ER- RAKÎB\n");
+            Console.Write("13. EL- BASÎR      46. EL- LATÎF         79. ER- RAÛF\n");
+            Console.Write("14. EL- BÂTIN      47. EL- MÂCİD         80. ER- RAZZÂK\n");
+            Console.Write("15. EL- BEDÎ       48. MÂLİK-ÜL MÜLK     81. ER- REŞÎD\n");
+            Console.Write("16. EL- BERR       49. EL- MÂNİ          82. ES- SABÛR\n");
+            Console.Write("17. EL- CÂMİ       50. EL- MECÎD         83. ES- SAMED\n");
+            Console.Write("18. EL- CEBBÂR     51. EL- MELİK         84. ES- SELÂM\n");
+            Console.Write("19. EL- CELÎL      52. EL- METÎN         85. ES- SEMİ\n");
+            Console.Write("20. ED- DÂRR       53. EL- MU'IZ         86. EŞ- ŞEHÎD\n");
+            Console.Write("21. EL- EVVEL      54. EL- MUAHHİR       87. EŞ- ŞEKÛR\n");
+            Console.Write("22. EL- FETTÂH     55. EL- MUCÎB         88. ET- TEVVÂB\n");
+            Console.Write("23. EL- GAFFÂR     56. EL- MUGNÎ         89. EL- VÂCİD\n");
+            Console.Write("24. EL- GAFÛR      57. EL- MUHSÎ         90. EL- VÂHİD\n");
+            Console.Write("25. EL- GANİYY     58. EL- MUHYÎ         91. EL- VÂLÎ\n");
+            Console.Write("26. EL- HABÎR      59. EL- MUÎD          92. EL- VÂRİS\n");
+            Console.Write("27. EL- HÂDÎ       60. EL- MUKADDİM      93. EL- VÂSİ\n");
+            Console.Write("28. EL- HÂFID      61. EL- MUKÎT         94. EL- VEDÛD\n");
+            Console.Write("29. EL- HAFÎZ      62. EL- MUKSİT        95. EL- VEHHÂB\n");
+            Console.Write("30. EL- HAK        63. EL- MUKTEDİR      96. EL- VEKÎL\n");
+            Console.Write("31. EL- HAKEM      64. EL- MUSAVVİR      97. EL- VELİYY\n");
+            Console.Write("32. EL- HAKÎM      65. EL- MÜ'MİN        98. EZ- ZÂHİR\n");
+            Console.Write("33. EL- HÂLIK      66. EL- MÜBDİ         99. ZÜL-CELÂLİ VEL İKRÂM\n");
+            yeni:
+            Console.Write("\nMânasını görmek istediğiniz ismi yazınız: ");
+            string isim = Console.ReadLine();
+            if (isim == "ALLAH")
+                Console.Write("Her ismin vasfını ihtiva eden öz adı. Kendinden başka ilah bulunmayan tek ALLAH. Bu ism-i şerif, Cenâb-ı Hakk'ın has ismidir. Bu itibarla diğer isimlerin ifade ettiği bütün güzel vasıfları ve İlâhî sıfatları içine alır. Diğer isimler ise, yalnız kendi mânalarına delâlet ederler. Bu bakımdan ALLAH isminin yerini hiçbir isim tutamaz. Bu isim, ALLAH'tan başkasına mecazen de verilemez.\n");
+            else if (isim == "EL- ADL")
+                Console.Write("Mutlak adil, yerli yerinde yapan.\n");
+            else if (isim == "EL- AFÜVV")
+                Console.Write("Affı çok olan, günahları mağfiret eden.\n");
+            else if (isim == "EL- ÂHİR")
+                Console.Write("Ebedi olan, varlığının sonu olmayan.\n");
+            else if (isim == "EL- ALÎM")
+                Console.Write("Gizli açık, geçmiş, gelecek, her şeyi, ezeli ve ebedi ilmi ile en mükemmel bilen.\n");
+            else if (isim == "EL- ALİYY")
+                Console.Write("Yüceler yücesi, çok yüce olan.\n");
+            else if (isim == "EL- AZÎM")
+                Console.Write("Büyüklükte benzeri yok. Pek yüce olan.\n");
+            else if (isim == "EL- AZÎZ")
+                Console.Write("İzzet sahibi, her şeye galip olan, karşı gelinemeyen.\n");
+            else if (isim == "EL- BÂİS")
+                Console.Write("Mahşerde ölüleri dirilten, peygamber gönderen.\n");
+            else if (isim == "EL- BÂKÎ")
+                Console.Write("Varlığının sonu olmayan, ebedi olan.\n");
+            else if (isim == "EL- BÂRİ")
+                Console.Write("Her şeyi kusursuz ve mütenasip yaratan.\n");
+            else if (isim == "EL- BÂSIT")
+                Console.Write("Dilediğinin rızkını genişleten, ruhları veren.\n");
+            else if (isim == "EL- BASÎR")
+                Console.Write("Gizli açık, her şeyi en iyi gören.\n");
+            else if (isim == "EL- BÂTIN")
+                Console.Write("Aklın tasavvurundan gizli olan.\n");
+            else if (isim == "EL- BEDÎ")
+                Console.Write("Misalsiz, örneksiz harikalar yaratan (eşi ve benzeri olmayan).\n");
+            else if (isim == "EL- BERR")
+                Console.Write("İyilik ve ihsanı bol olan.\n");
+            else if (isim == "EL- CÂMİ")
+                Console.Write("İki zıttı bir arada bulunduran. Kıyamette her mahlûkatı bir araya toplayan.\n");
+            else if (isim == "EL- CEBBÂR")
+                Console.Write("Azamet ve kudret sahibi. Dilediğini yapan ve yaptıran. Hükmüne karşı gelinemeyen.\n");
+            else if (isim == "EL- CELÎL")
+                Console.Write("Celal ve azamet sahibi olan.\n");
+            else if (isim == "ED- DÂRR")
+                Console.Write("Elem, zarar verenleri yaratan.\n");
+            else if (isim == "EL- EVVEL")
+                Console.Write("Ezeli olan, varlığının başlangıcı olmayan.\n");
+            else if (isim == "EL- FETTÂH")
+                Console.Write("Her türlü sıkıntıları gideren.\n");
+            else if (isim == "EL- GAFFÂR")
+                Console.Write("Günahları örten ve çok mağfiret eden. Dilediğini günah işlemekten koruyan.\n");
+            else if (isim == "EL- GAFÛR")
+                Console.Write("Affı, mağfireti bol olan.\n");
+            else if (isim == "EL- GANİYY")
+                Console.Write("İhtiyaçsız, muhtaç olmayan, her şey kendisine muhtaç olan.\n");
+            else if (isim == "EL- HABÎR")
+                Console.Write("Her şeyden haberdar. Her şeyin gizli taraflarından haberi olan.\n");
+            else if (isim == "EL- HÂDÎ")
+                Console.Write("Hidayet veren.\n");
+            else if (isim == "EL- HÂFID")
+                Console.Write("Kâfir ve facirleri alçaltan.\n");
+            else if (isim == "EL- HAFÎZ")
+                Console.Write("Her şeyi koruyucu olan.\n");
+            else if (isim == "EL- HAK")
+                Console.Write("Varlığı hiç değişmeden duran. Var olan, hakkı ortaya çıkaran.\n");
+            else if (isim == "EL- HAKEM")
+                Console.Write("Mutlak hakim, hakkı bâtıldan ayıran. Hikmet sahibi olan.\n");
+            else if (isim == "EL- HAKÎM")
+                Console.Write("Her işi hikmetli, her şeyi hikmetle yaratan.\n");
+            else if (isim == "EL- HÂLIK")
+                Console.Write("Yaratan, yoktan var eden. Varlıkların geçireceği halleri takdir eden.\n");
+            else if (isim == "EL- HALÎM")
+                Console.Write("Cezada, acele etmeyen, yumuşak davranan, hilm sahibi olan.\n");
+            else if (isim == "EL- HAMÎD")
+                Console.Write("Her türlü hamd ve senaya layık olan.\n");
+            else if (isim == "EL- HASÎB")
+                Console.Write("Kulların hesabını en iyi gören.\n");
+            else if (isim == "EL- HAYY")
+                Console.Write("Ezeli ve ebedi bir hayat ile diri olan.\n");
+            else if (isim == "EL- KÂBID")
+                Console.Write("Dilediğinin rızkını daraltan, ruhları alan.\n");
+            else if (isim == "EL- KÂDİR")
+                Console.Write("Dilediğini dilediği gibi yaratmaya muktedir olan.\n");
+            else if (isim == "EL- KAHHÂR")
+                Console.Write("Her istediğini yapacak güçte olan, galip ve hâkim olan.\n");
+            else if (isim == "EL- KAVİYY")
+                Console.Write("Kudreti en üstün ve hiç azalmaz olan.\n");
+            else if (isim == "EL- KAYYÛM")
+                Console.Write("Mahlukları varlıkta durduran, zatı ile kaim olan.\n");
+            else if (isim == "EL- KEBÎR")
+                Console.Write("Büyüklükte benzeri yok, pek büyük olan.\n");
+            else if (isim == "EL- KERÎM")
+                Console.Write("Keremi, lütuf ve ihsânı bol, karşılıksız veren, çok ikram eden.\n");
+            else if (isim == "EL- KUDDÛS")
+                Console.Write("Her noksanlıktan uzak ve her türlü takdîse lâyık olan.\n");
+            else if (isim == "EL- LATÎF")
+                Console.Write("Her şeye vakıf, lütuf ve ihsan sahibi olan.\n");
+            else if (isim == "EL- MÂCİD")
+                Console.Write("Kadri ve şânı büyük, keremi, ihsanı bol olan.\n");
+            else if (isim == "MÂLİK-ÜL MÜLK")
+                Console.Write("Mülkün, her varlığın sahibi olan.\n");
+            else if (isim == "EL- MÂNİ")
+                Console.Write("Dilemediği şeye mani olan, engelleyen.\n");
+            else if (isim == "EL- MECÎD")
+                Console.Write("Nimeti, ihsanı sonsuz, şerefi çok üstün, her türlü övgüye layık bulunan.\n");
+            else if (isim == "EL- MELİK")
+                Console.Write("Mülkün, kâinatın sahibi, mülk ve saltanatı devamlı olan.\n");
+            else if (isim == "EL- METÎN")
+                Console.Write("Kuvvet ve kudret menbaı, pek güçlü olan.\n");
+            else if (isim == "EL- MU'IZ")
+                Console.Write("Dilediğini aziz eden.\n");
+            else if (isim == "EL- MUAHHİR")
+                Console.Write("Dilediğini alçaltan, sona, geriye bırakan.\n");
+            else if (isim == "EL- MUCÎB")
+                Console.Write("Duaları, istekleri kabul eden.\n");
+            else if (isim == "EL- MUGNÎ")
+                Console.Write("Müstağni kılan. İhtiyaç gideren, zengin eden.\n");
+            else if (isim == "EL- MUHSÎ")
+                Console.Write("Yarattığı ve yaratacağı bütün varlıkların sayısını bilen.\n");
+            else if (isim == "EL- MUHYÎ")
+                Console.Write("İhya eden, yarattıklarına can veren.\n");
+            else if (isim == "EL- MUÎD")
+                Console.Write("Yarattıklarını yok edip, sonra tekrar diriltecek olan.\n");
+            else if (isim == "EL- MUKADDİM")
+                Console.Write("Dilediğini yükselten, öne geçiren, öne alan.\n");
+            else if (isim == "EL- MUKÎT")
+                Console.Write("Rızıkları yaratan.\n");
+            else if (isim == "EL- MUKSİT")
+                Console.Write("Mazlumların hakkını alan, adaletle hükmeden, her işi birbirine uygun yapan.\n");
+            else if (isim == "EL- MUKTEDİR")
+                Console.Write("Dilediği gibi tasarruf eden, her şeyi kolayca yaratan, kudret sahibi olan.\n");
+            else if (isim == "EL- MUSAVVİR")
+                Console.Write("Varlıklara şekil veren ve onları birbirinden farklı özellikte yaratan.\n");
+            else if (isim == "EL- MÜ'MİN")
+                Console.Write("Güven veren, emin kılan, koruyan, iman nurunu veren.\n");
+            else if (isim == "EL- MÜBDİ")
+                Console.Write("Maddesiz, örneksiz yaratan.\n");
+            else if (isim == "EL- MÜHEYMİN")
+                Console.Write("Her şeyi görüp gözeten, her varlığın yaptıklarından haberdar olan.\n");
+            else if (isim == "EL- MÜMÎT")
+                Console.Write("Her canlıya ölümü tattıran.\n");
+            else if (isim == "EL- MÜNTEKIM")
+                Console.Write("Asilerin, zalimlerin cezasını veren.\n");
+            else if (isim == "EL- MÜTEÂLÎ")
+                Console.Write("Son derece yüce olan.\n");
+            else if (isim == "EL- MÜTEKEBBİR")
+                Console.Write("Büyüklükte eşi, benzeri yok olan.\n");
+            else if (isim == "EL- MÜZİL")
+                Console.Write("Dilediğini zillete düşüren, hor ve hakir eden.\n");
+            else if (isim == "EN- NÂFİ")
+                Console.Write("Fayda veren şeyleri yaratan.\n");
+            else if (isim == "EN- NÛR")
+                Console.Write("Âlemleri nurlandıran, dilediğine nur veren.\n");
+            else if (isim == "ER- RÂFİ")
+                Console.Write("Şeref verip yükselten.\n");
+            else if (isim == "ER- RAHÎM")
+                Console.Write("Ahirette, sadece müminlere acıyan, merhamet eden.\n");
+            else if (isim == "ER- RAHMÂN")
+                Console.Write("Dünyada bütün mahlûkata merhamet eden, şefkat gösteren, ihsan eden.\n");
+            else if (isim == "ER- RAKÎB")
+                Console.Write("Her varlığı, her işi her an gözeten. Bütün işleri murakabesi altında bulunduran.\n");
+            else if (isim == "ER- RAÛF")
+                Console.Write("Çok merhametli, pek şefkatli olan.\n");
+            else if (isim == "ER- RAZZÂK")
+                Console.Write("Her varlığın rızkını veren ve ihtiyacını karşılayan.\n");
+            else if (isim == "ER- REŞÎD")
+                Console.Write("İrşada muhtaç olmayan, doğru yolu gösteren.\n");
+            else if (isim == "ES- SABÛR")
+                Console.Write("Ceza vermede, acele etmeyen.\n");
+            else if (isim == "ES- SAMED")
+                Console.Write("Hiçbir şeye ihtiyacı olmayan, herkesin muhtaç olduğu merci olan.\n");
+            else if (isim == "ES- SELÂM")
+                Console.Write("Her türlü tehlikelerden selamete çıkaran. Cennetteki bahtiyar kullarına selâm eden.\n");
+            else if (isim == "ES- SEMİ")
+                Console.Write("Her şeyi en iyi işiten, duaları kabul eden.\n");
+            else if (isim == "EŞ- ŞEHÎD")
+                Console.Write("Zamansız, mekansız hiçbir yerde olmayarak her zaman her yerde hazır ve nazır olan.\n");
+            else if (isim == "EŞ- ŞEKÛR")
+                Console.Write("Az amele, çok sevap veren.\n");
+            else if (isim == "ET- TEVVÂB")
+                Console.Write("Tevbeleri kabul edip, günahları bağışlayan.\n");
+            else if (isim == "EL- VÂCİD")
+                Console.Write("Kendisinden hiçbir şey gizli kalmayan, hiçbir şeye muhtaç olmayan.\n");
+            else if (isim == "EL- VÂHİD")
+                Console.Write("Zat, sıfat ve fiillerinde benzeri ve ortağı olmayan, tek olan.\n");
+            else if (isim == "EL- VÂLÎ")
+                Console.Write("Bütün kâinatı idare eden, onların işlerini yoluna koyan.\n");
+            else if (isim == "EL- VÂRİS")
+                Console.Write("Her şeyin asıl sahibi olan.\n");
+            else if (isim == "EL- VÂSİ")
+                Console.Write("Rahmet ve kudret sahibi, ilmi ile her şeyi ihata eden.\n");
+            else if (isim == "EL- VEDÛD")
+                Console.Write("İyiliği seven, iyilik edene ihsan eden. Sevgiye layık olan.\n");
+            else if (isim == "EL- VEHHÂB")
+                Console.Write("Karşılıksız nimetler veren, çok fazla ihsan eden.\n");
+            else if (isim == "EL- VEKÎL")
+                Console.Write("Kulların işlerini bitiren. Kendisine tevekkül edenlerin işlerini en iyi neticeye ulaştıran.\n");
+            else if (isim == "EL- VELİYY")
+                Console.Write("Müslümanların dostu, onları sevip yardım eden.\n");
+            else if (isim == "EZ- ZÂHİR")
+                Console.Write("Yarattıkları ile varlığı açık, aşikâr olan, kesin delillerle bilinen.\n");
+            else if (isim == "ZÜL-CELÂLİ VEL İKRÂM")
+                Console.Write("Celal, azamet, şeref, kemal ve ikram sahibi olan.\n");
+            else
+            {
+                Console.Write("Mânasını görmek istediğiniz ismin harflerini yukarıda gördüğünüz biçimiyle doğru olarak yazınız!\n");
+                goto yeni;
+            }
+            yeni2:
+            Console.Write("Başka bir ismin mânasını görmek ister misiniz?\n");
+            Console.Write("Evet > E\n");
+            Console.Write("Hayır > H\n");
+            string cevap = Console.ReadLine().ToUpper();
+            if (cevap == "E")
+                goto yeni;
+            if (cevap == "H")
+            {
+                Console.Write("Uygulama sonlandı, uygulamayı kapamak için bir tuşa basınız.\n");
+                Console.Write("©an ÖZEN © 2018\n");
+            }
+            else
+            {
+                Console.Write("Cevabınızı e veya h olarak yazınız!\n");
+                goto yeni2;
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
